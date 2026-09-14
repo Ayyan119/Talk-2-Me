e265d1282e69b5cc40b9334f5806f80bf0bb43ca
Ayyan Shahid <151453442+Ayyan119@users.noreply.github.com>
2026-09-14 21:48:02 +0500

    feat(stt): add deepgram sdk and dependencies

--- src/stt/__init__.py
+++ src/stt/__init__.py
@@ -1,10 +1,6 @@
-"""Module: stt
-Layer: Core/Domain
-Purpose: Speech-to-Text module domain interfaces.
-Dependencies: src.stt.base
-"""
-
 from src.stt.base import SpeechToText
+from src.stt.deepgram_stt import DeepgramSTT
 from src.stt.faster_whisper_stt import FasterWhisperSTT
+from src.stt.openai_whisper_stt import OpenAIWhisperSTT
 
-__all__ = ["FasterWhisperSTT", "SpeechToText"]
+__all__ = ["DeepgramSTT", "FasterWhisperSTT", "OpenAIWhisperSTT", "SpeechToText"]
